0f6aca30881f9bca46bb97ab294a879b1bd30994
ElsaAwal <116138681+ElsaAwal@users.noreply.github.com>
2022-10-19 12:37:53 +0700

    SIPOSYANDU
    Sistem informasi rekam medis data 
    peserta posyandu berbasis website (studi kasus: Posyandu Kemuning Desa 
    Gajah)
    Project Tugas Akhir sebagai salah satu syarat untuk menyelesaikan Studi Program Studi Manajemen Informatika Jenjang Program Diploma III

---
+++ index.php
@@ -0,0 +1,258 @@
+<?php
+    session_start();
+    include 'konek.php';
+    $level = "admin";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>SIPOSYANDU</title>
+    <!-- core CSS -->
+    <link href="main/css/bootstrap.min.css" rel="stylesheet">
+    <link href="main/css/font-awesome.min.css" rel="stylesheet">
+    <link href="main/css/animate.min.css" rel="stylesheet">
+    <link href="main/css/owl.carousel.css" rel="stylesheet">
+    <link href="main/css/owl.transitions.css" rel="stylesheet">
+    <link href="main/css/prettyPhoto.css" rel="stylesheet">
+    <link href="main/css/main.css" rel="stylesheet">
+    <link href="main/css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head>
+<!--/head-->
+
+<body id="home" class="homepage">
+   
+    <header id="header">
+        <nav id="main-menu" class="navbar navbar-default navbar-fixed-top" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><img src="main/img/logoposyandu.jpg" width="89" height="64" alt="logo"></a>
+                </div>
+
+                <div class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li class="scroll active"><a href="#home">HOME</a></li>
+                        <li class="scroll"><a href="#features">Jadwal</a></li>
+                        <li class="scroll"><a href="#services">Informasi</a></li>
+                        <li class="scroll"><a href="petugas.php">Petugas</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!--/.container-->
+        </nav>
+        <!--/nav-->
+    </header>
+    <!--/header-->
+
+    <section id="cta2">
+        <div class="container">
+            <div class="text-center">
+                <h2 class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms"><span>SISTEM</span> INFORMASI POSYANDU <br> ...</h2>
+                <p class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="100ms">Klik Login untuk pencatatan data peserta Posyandu
+                <div class="row justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <div class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="200ms">
+                           <!-- Button trigger modal -->
+                           <a href="login.php" type="submit" class="btn btn-primary">Login</a>                          
+                        </div>
+                    </div>
+                </div>
+                <img class="img-responsive wow fadeIn" src="main/images/cta2/cta2-img.png" alt="" data-wow-duration="300ms" data-wow-delay="300ms">
+            </div>
+        </div>
+    </section>
+
+    <section id="features">
+        <div class="container">
+            <div class="section-header">
+                <h2 class="section-title text-center wow fadeInDown">Agenda Kegiatan Posyandu</h2>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 wow fadeInLeft">
+                    <img class="img-responsive" src="main/img/attendance.png" width="370" height="370" alt="">
+                </div>
+                <div class="col-sm-6">
+                    <div class="media service-box wow fadeInRight">
+                        <div class="pull-left">
+                            <img src="main/img/clock.png" alt="">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Setiap tanggal 20 per bulan</h4>
+                            <p>09.00 - 11.00 WIB</p>
+                            <p>Posyandu Balita</p>
+                        </div>
+                    </div>
+
+                    <div class="media service-box wow fadeInRight">
+                        <div class="pull-left">
+                            <img src="main/img/clock.png" alt="">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Setiap tanggal 16 per bulan</h4>
+                            <p>09.00 - 11.00 WIB</p>
+                            <p>Posyandu PUS (Pasangan Usia Subur)</p>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div class="container">
+
+            <div class="section-header">
+                <h2 class="section-title text-center wow fadeInDown">Manfaat Posyandu</h2>
+            </div>
+            <div class="row">
+                <div class="features">
+                    <div class="col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms">
+                        <div class="media service-box">
+                            <div class="pull-left">
+                                <img src="main/img/number.png" alt="">
+                            </div>
+                            <div class="media-body">
+                                <p>Perbaikan Perilaku, Keadaan Gizi dan Kesehatan keluarga.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/.col-md-4-->
+
+                    <div class="col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="100ms">
+                        <div class="media service-box">
+                            <div class="pull-left">
+                                <img src="main/img/number5.png" width="33" height="33" alt="">
+                            </div>
+                            <div class="media-body">
+                                <p>Mendukung Perilaku hidup bersih dan Sehat.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/.col-md-4-->
+                    </div>
+                    <!--/.col-md-4-->
+                </div>
+            <div class="row">
+                <div class="features">
+                    <div class="col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms">
+                        <div class="media service-box">
+                            <div class="pull-left">
+                                <img src="main/img/number2.png" alt="">
+                            </div>
+                            <div class="media-body">
+                                <p>Pencegahan Penyakit Yang Berbasis Lingkungan.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/.col-md-4-->
+
+                    <div class="col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="100ms">
+                        <div class="media service-box">
+                            <div class="pull-left">
+                                <img src="main/img/number6.png" width="40" height="40" alt="">
+                            </div>
+                            <div class="media-body">
+                                <p>Pencegahan Penyakit Dengan Imunisasi.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/.col-md-4-->
+                    </div>
+                    <!--/.col-md-4-->
+                </div>
+            <div class="row">
+                <div class="features">
+                    <div class="col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms">
+                        <div class="media service-box">
+                            <div class="pull-left">
+                                <img src="main/img/number3.png" alt="">
+                            </div>
+                            <div class="media-body">
+                                <p>Mendukung Pelayanan Keluarga Berencana.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/.col-md-4-->
+
+                    <div class="col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="100ms">
+                        <div class="media service-box">
+                            <div class="pull-left">
+                                <img src="main/img/number7.png" width="30" height="30" alt="">
+                            </div>
+                            <div class="media-body">
+                                <p>Mendukung Pemberdayaan keluarga dan Masyarakat.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/.col-md-4-->
+                    </div>
+                    <!--/.col-md-4-->
+                </div>
+            <div class="row">
+                <div class="features">
+                    <div class="col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms">
+                        <div class="media service-box">
+                            <div class="pull-left">
+                                <img src="main/img/number4.png" alt="">
+                            </div>
+                            <div class="media-body">
+                                <p>Pusat Informasi dan Konseling dalam Perlindungan Anak.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/.col-md-4-->
+                    </div>
+                    <!--/.col-md-4-->
+                </div>
+            </div>
+            <!--/.row-->
+        </div>
+        <!--/.container-->
+    </section>
+    <!--/#services-->
+
+    <script src="main/js/jquery.js"></script>
+    <script src="main/js/bootstrap.min.js"></script>
+    <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+    <script src="main/js/owl.carousel.min.js"></script>
+    <script src="main/js/mousescroll.js"></script>
+    <script src="main/js/smoothscroll.js"></script>
+    <script src="main/js/jquery.prettyPhoto.js"></script>
+    <script src="main/js/jquery.isotope.min.js"></script>
+    <script src="main/js/jquery.inview.min.js"></script>
+    <script src="main/js/wow.min.js"></script>
+    <script src="main/js/main.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <!-- Swal -->
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9.15.2/dist/sweetalert2.all.min.js"></script>
+	<!-- Optional: include a polyfill for ES6 Promises for IE11 -->
+	<script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script>
+</body>
+
+</html>
